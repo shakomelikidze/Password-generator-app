18c4c79f27c38c1c099c8d6815da55c4c19bf7fd
shakomelikidze <shako456@gmail.com>
2023-09-17 18:40:54 +0400

    add checkbox functions

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ const numbers = document.querySelector('.numbers');
 const symbols = document.querySelector('.symbols');
 const tooWeak = document.querySelector('.too-weak');
 const weak = document.querySelector('.weak');
+const medium = document.querySelector('.medium');
+const strong = document.querySelector('.strong');
 const passwordResult = document.querySelector('.password');
 const generateBtn = document.querySelector('.generate-button');
 const passwordStrength = document.querySelector('.password-strength-scale-p');
+const checkboxes = document.querySelectorAll('.checkbox-input');
 
 // Copy button
 copyBtn.addEventListener('click', ()=> {
@@ -37,71 +40,75 @@ slider.addEventListener("input", () => {
       "%, #18171F 100%)";
 });
 
-// Generate button
+// Generate password
 generateBtn.addEventListener('click', ()=> {
-    generate();
+    passwordGenerator();
 });
 
-function generate() {
+function passwordGenerator() {
+    copyBtn.classList.add = "position-relative";
+    let characterSet = "";
+  
     if (uppercaseLetters.checked) {
-        tooWeak.style.backgroundColor = '#F64A4A';
-        tooWeak.style.border = '#F64A4A'
-        passwordStrength.innerHTML = 'TOO WEAK!'
+      characterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
-
-    if (uppercaseLetters.checked && lowercaseLetters.checked) {
-        tooWeak.style.backgroundColor = '#FB7C58';
-        tooWeak.style.border = '#FB7C58'
-        weak.style.backgroundColor = '#FB7C58';
-        weak.style.border = '#FB7C58'
-        passwordStrength.innerHTML = 'WEAK'
+    if (lowercaseLetters.checked) {
+      characterSet += "abcdefghijklmnopqrstuvwxyz";
     }
-
+    if (numbers.checked) {
+      characterSet += "0123456789";
+    }
+    if (symbols.checked) {
+      characterSet += "~!@#$%^&()_+{}[]|:;<>,>./";
+    }
+  
+    let randomPassowrd = "";
+  
+    if (
+      !uppercaseLetters.checked &&
+      !lowercaseLetters.checked &&
+      !numbers.checked &&
+      !symbols.checked
+    ) {
+      passwordResult.innerHTML = "";
+      return;
+    }
+  
+    for (let i = 0; i < slider.value; i++) {
+      const randomIndex = Math.floor(Math.random() * characterSet.length);
+      randomPassowrd += characterSet[randomIndex];
+    }
+    passwordResult.innerText = randomPassowrd;
 }
 
+// Change rectangle colors
+
+// too-weak: #F64A4A
+// weak: #FB7C58
+// medium: #F8CD65
+// strong: #A4FFAF
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// uppercaseLetters.addEventListener('click', ()=> {
-//     if (uppercaseLetters.checked) {
-//         tooWeak.style.backgroundColor = '#F64A4A';
-//         tooWeak.style.border = '#F64A4A'
-//     } else {
-//         tooWeak.style.backgroundColor = '#18171F';
-//         tooWeak.style.border = '2px solid #E6E5EA';
-//     }
+// generateBtn.addEventListener('click', ()=> {
+    // generate();
 // });
 
-// lowercaseLetters.addEventListener('click', ()=> {
-//     if (uppercaseLetters.checked && lowercaseLetters.checked) {
-//         tooWeak.style.backgroundColor = '#FB7C58';
-//         tooWeak.style.border = '#FB7C58'
-//         weak.style.backgroundColor = '#FB7C58';
-//         weak.style.border = '#FB7C58'
-//     } else {
-//         weak.style.backgroundColor = '#18171F';
-//         weak.style.border = '2px solid #E6E5EA';
-//     }
-//     if (lowercaseLetters.checked) {
-//         tooWeak.style.backgroundColor = '#F64A4A';
-//         tooWeak.style.border = '#F64A4A'
-//     } 
-// })
+// function countCheckedCheckboxes() {
+//     let checkedCount = 0;
+//     checkboxes.forEach(checkbox => {
+//         if (checkbox.checked) {
+//             checkedCount++;
+//         }
+//     });
+//     return checkedCount;
+// }
+//     checkboxes.forEach(checkbox => {
+//     checkbox.addEventListener('click', () => {
+//         const checkedCount = countCheckedCheckboxes();
+//         console.log(checkedCount);
+//     });
+// });
 
 
 
@@ -123,50 +130,65 @@ function generate() {
 
 
 
-
-
-
-
-
-
-
-
-
-// Generate password
-// function passwordGenerator() {
-//     // copyBtn.style.display = "none";
-//     let characterSet = "";
-  
-//     if (uppercaseLetters.checked) {
-//       characterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//       console.log(uppercaseLetters.checked);
-//     }
-//     if (lowercaseLetters.checked) {
-//       characterSet += "abcdefghijklmnopqrstuvwxyz";
-//     }
-//     if (numbers.checked) {
-//       characterSet += "0123456789";
-//     }
-//     if (symbols.checked) {
-//       characterSet += "~!@#$%^&()_+{}[]|:;<>,>./";
-//     }
-  
-//     let randomPassowrd = "";
-  
-//     if (
-//       !uppercaseLetters.checked &&
-//       !lowercaseLetters.checked &&
-//       !numbers.checked &&
-//       !symbols.checked
-//     ) {
-//       passwordResult.innerHTML = "";
-//       return;
-//     }
-  
-//     for (let i = 0; i < slider.value; i++) {
-//       const randomIndex = Math.floor(Math.random() * characterSet.length);
-//       randomPassowrd += characterSet[randomIndex];
-//     }
-//     passwordResult.innerText = randomPassowrd;
+//   if (checkedCount == 1) {
+//     tooWeak.style.backgroundColor = '#F64A4A';
+//     tooWeak.style.border = '#F64A4A';
+//     passwordStrength.innerHTML = 'TOO WEAK!';
+//     weak.style.backgroundColor = '#18171F';
+//     weak.style.borderWidth = '2px';
+//     weak.style.borderColor = '#E6E5EA';
 //   }
+//   if (checkedCount == 2) {
+//     tooWeak.style.backgroundColor = '#FB7C58';
+//     tooWeak.style.border = '#FB7C58'
+//     weak.style.backgroundColor = '#FB7C58';
+//     weak.style.border = '#FB7C58';
+//     passwordStrength.innerHTML = 'WEAK';
+//   }
+//   if (checkedCount == 3) {
+//     tooWeak.style.backgroundColor = ' #F8CD65';
+//     tooWeak.style.border = ' #F8CD65';
+//     weak.style.backgroundColor = ' #F8CD65';
+//     weak.style.border = ' #F8CD65';
+//     medium.style.backgroundColor = ' #F8CD65';
+//     medium.style.border = ' #F8CD65';
+//     passwordStrength.innerHTML = 'MEDIUM';
+// }
+// if (checkedCount == 4) {
+//   tooWeak.style.backgroundColor = '#A4FFAF';
+//   tooWeak.style.border = '#A4FFAF';
+//   weak.style.backgroundColor = '#A4FFAF';
+//   weak.style.border = ' #A4FFAF';
+//   medium.style.backgroundColor = ' #A4FFAF';
+//   medium.style.border = ' #A4FFAF';
+//   strong.style.backgroundColor = '#A4FFAF';
+//   strong.style.border = '#A4FFAF';
+//   passwordStrength.innerHTML = 'STRONG';
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
